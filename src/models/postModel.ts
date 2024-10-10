67afbcf07fc8f2268c023ee3fee40ffad661dbb1
yhuda <strny3480@gmail.com>
2024-10-10 11:52:17 +0300

    edited the schemas

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -2,19 +2,32 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 
 export interface IComment {
   content: string;
-  author: Types.ObjectId;
+  author?: Types.ObjectId;
   createdAt: Date;
 }
 
 export interface IPost extends Document {
-  _id: Types.ObjectId;
   title: string;
   content: string;
-  author: Types.ObjectId;
+  author?: Types.ObjectId;
   comments: IComment[];
 }
 
-const CommentSchema = new Schema<IComment>({});
+const CommentSchema = new Schema<IComment>({
+  content: { type: String, required: true },
+  author: { type: Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
+}, {
+  timestamps: true
+});
 
+const PostSchema = new Schema<IPost>({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: Types.ObjectId, ref: "User", required: true },
+  comments: [CommentSchema],
+}, {
+  timestamps: true
+});
 
 export default mongoose.model<IPost>("Post", PostSchema);
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,7 +2,6 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 import validator from "validator";
 
 export interface IUser extends Document {
-  _id: Types.ObjectId;
   username: string;
   email: string;
   profile: {
@@ -12,6 +11,30 @@ export interface IUser extends Document {
   posts: Types.ObjectId[];
 }
 
-const UserSchema = new Schema<IUser>({});
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: [true, "Username is required."],
+    unique: true,
+    lowercase: true,
+    validate: function (value: string) {
+     validator.isAlphanumeric(value), "Username can only contain alphanumeric characters."
+    },
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: true,
+    lowercase: true,
+    validate: function (value: string) {
+     validator.isEmail(value), "Invalid email format."
+    },
+  },
+  profile: {
+    bio: String,
+    socialLinks: [String],
+  },
+  posts: [{ type: Types.ObjectId, ref: "Post" }],
+});
 
 export default mongoose.model<IUser>("User", UserSchema);
